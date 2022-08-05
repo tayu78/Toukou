2be7701409b82095c50062d4c0c80b9b971dcd25
Yuya Satake <sata.yu.7878@gmail.com>
2022-08-05 05:16:20 -0700

    added readme

--- client/src/cons/cons.js
+++ client/src/cons/cons.js
@@ -1,2 +1,2 @@
-// export const SERVER_DOMAIN = "https://floating-ravine-24176.herokuapp.com";
-export const SERVER_DOMAIN = "http://localhost:8080";
+export const SERVER_DOMAIN = "https://floating-ravine-24176.herokuapp.com";
+// export const SERVER_DOMAIN = "http://localhost:8080";
